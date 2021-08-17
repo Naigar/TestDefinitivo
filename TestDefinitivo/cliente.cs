6a1ec9d29b485dd32ff5cfbbfb5f18c9b790790e
alirio1998 <62248516+alirio1998@users.noreply.github.com>
2021-08-17 10:52:19 -0400

    Alirio (#4)
    * crear funcion prueba
    
    * cambio

--- TestDefinitivo/cliente.cs
+++ TestDefinitivo/cliente.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -11,6 +11,7 @@ namespace TestDefinitivo
         public void prueba()
         {
 
+//prueba
         }
     }
 }
