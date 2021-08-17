7f1640dd530818da9d7eba1dba6496d3c5fafb91
Ignacio Andres Gomez Ramirez <i.gomez@outlook.cl>
2021-08-17 10:09:05 -0400

    Agregar archivos de proyecto.

---
+++ TestDefinitivo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestDefinitivo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
